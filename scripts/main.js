0a16f44dfc7aecaddd1dfb3bc1fdd1ae80bc4af2
Farzad I. Student <theeonsamurai@gmail.com>
2023-12-06 22:40:18 -0500

    w

--- scripts/main.js
+++ scripts/main.js
@@ -167,90 +167,45 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-  }
-  /*
-  // Confirmation dialog for delete account
-  const deleteAccountForm = document.getElementById('delete-account-form');
-  if (deleteAccountForm) {
-    deleteAccountForm.addEventListener('submit', function (event) {
-      if (!confirm('Are you sure you want to delete your account? This action cannot be undone.')) {
-        event.preventDefault();
-      }
-    });
-  }
+    // Get the modal
+    var modal = document.getElementById("myModal");
 
-  // Confirmation dialog for delete item
-  const deleteItemForm = document.getElementById('delete-item-form');
-  if (deleteItemForm) {
-    deleteItemForm.addEventListener('submit', function (event) {
-      if (!confirm('Are you sure you want to delete this item? This action cannot be undone.')) {
-        event.preventDefault();
-      }
-    });
-  }
+    // Get the button that opens the modal
+    var listLink = document.getElementById("preview");
 
-  // Modal window functionality for viewing list items
-  // Add event listeners to list items to open them in a modal window
-  const listItems = document.querySelectorAll('.list-item-link');
-  listItems.forEach(function (item) {
-    item.addEventListener('click', function (event) {
-      event.preventDefault();
-      const itemId = this.getAttribute('data-item-id');
-      // Fetch the content for the item and display it in a modal window
-      // This is a placeholder for the actual implementation
-      console.log('Open item in modal window:', itemId);
-    });
-  });
-});
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// Get the modal
-var modal = document.getElementById('myModal');
+    // When the user clicks on the button, open the modal
+    listLink.addEventListener('click', showModalPopUp);
+    
+    function showModalPopUp() {
+      $('#myModal').modal('show');
+      var options = {
+        url: listLink.dataset.content, //Set the url of the page
+        title: 'View Item', //Set the title for the pop up
+        allowMaximize: false,
+        showClose: true,
+      width: 600,
+      height: 400
+    };
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
-
-// Function to open the modal
-function openModal(itemId) {
-  fetch('/path-to-your-endpoint?itemId=' + itemId) // Update with your actual endpoint
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    //Invoke the modal dialog by passing in the options array variable
+    SP.SOD.execute('sp.ui.dialog.js', 'SP.UI.ModalDialog.showModalDialog', options);
     }
-    return response.text();
-  })
-  .then(html => {
-    document.getElementById('modal-body').innerHTML = html;
-    modal.style.display = 'block';
-  })
-  .catch(error => {
-    console.error('Error fetching item details:', error);
-    // Handle the error, e.g., by showing an error message to the user
-  });
-}
 
-// Function to close the modal
-function closeModal() {
-  modal.style.display = 'none';
-}
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  closeModal();
-}
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    closeModal();
+
   }
-}
-
-// Add event listeners to list items to open them in a modal window
-const listItems = document.querySelectorAll('.list-item-link');
-listItems.forEach(function (item) {
-  item.addEventListener('click', function (event) {
-    event.preventDefault();
-    const itemId = this.getAttribute('data-item-id');
-    openModal(itemId);
-  });*/
 });
